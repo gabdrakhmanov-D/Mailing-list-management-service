7179a8945b8955bc0261ddefcf6e0c808201e007
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-17 18:28:30 +0500

    создана Модель «Сообщение»

--- message_sender/models.py
+++ message_sender/models.py
@@ -18,3 +18,14 @@ class MailingRecipient(models.Model):
                                null=False,
                                verbose_name= "Комментарий")
 
+
+class Message(models.Model):
+
+    subject_line = models.CharField(max_length=250,
+                                    blank=False,
+                                    null=False,
+                                    verbose_name= "Тема письма")
+
+    message = models.TextField(blank=False,
+                               null=False,
+                               verbose_name= "Тело письма")
