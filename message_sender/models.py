f31af96fc66676fe074473d6f77148e7caafd10a
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-17 18:24:18 +0500

    создана Модель «Получатель рассылки»

--- message_sender/models.py
+++ message_sender/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class MailingRecipient(models.Model):
+
+    email = models.CharField(max_length=150,
+                             unique=True,
+                             blank=False,
+                             null=False,
+                             verbose_name= "Получатель рассылки")
+
+    fullname = models.CharField(max_length=250,
+                                blank=False,
+                                null=False,
+                                verbose_name= "Ф. И. О.")
+
+    comment = models.TextField(blank=False,
+                               null=False,
+                               verbose_name= "Комментарий")
+
