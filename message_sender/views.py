dab5df786411ceb901ec4aca7f49dd68c053fd60
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-17 19:46:47 +0500

    созданы классы представления для возможности добавлять, просматривать, редактировать и удалять получателей рассылки.

--- message_sender/views.py
+++ message_sender/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView
 from .models import MailingRecipient
 # Create your views here.
 
 
 class HomeView(TemplateView):
-    model = MailingRecipient
     template_name = 'message_sender/home.html'
+
+
+class RecipientCreate(CreateView):
+    model = MailingRecipient
+    fields = ['email', 'fullname', 'comment']
+    template_name = 'message_sender/add_recipient.html'
+    success_url = reverse_lazy('recipient_list')
+
+
+class RecipientsListView(ListView):
+    model = MailingRecipient
+    template_name = 'message_sender/recipient_list.html'
+    context_object_name = 'recipients'
+
+
+class RecipientUpdateView(UpdateView):
+    model = MailingRecipient
+    fields = ['email', 'fullname', 'comment']
+    template_name = 'message_sender/edit_recipient.html'
+    success_url = reverse_lazy('recipient_list')
+
+
+class RecipientDeleteView(DeleteView):
+    model = MailingRecipient
+    template_name = 'message_sender/recipient_confirm_delete.html'
+    success_url = reverse_lazy('recipient_list')
