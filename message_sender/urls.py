3d2377eb8aa7878801c78c79f12ea11780a53c7e
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-18 23:43:22 +0500

    зарегистрированы пути для представлений

--- message_sender/urls.py
+++ message_sender/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-from .views import HomeView, RecipientCreate, RecipientsListView, RecipientDeleteView, RecipientUpdateView
+from views import *
 
 app_name = 'catalog'
 
@@ -10,4 +9,8 @@ urlpatterns = [
     path("home/list_recipients/add_recipient/", RecipientCreate.as_view(), name="add_recipient"),
     path("home/list_recipients/<int:pk>/edit_recipient/", RecipientUpdateView.as_view(), name="edit_recipient"),
     path("home/list_recipients/<int:pk>/recipient_confirm_delete/", RecipientDeleteView.as_view(), name="recipient_delete"),
+    path("home/messages_list/", MessagesListView.as_view(), name="messages"),
+    path("home/messages_list/add_message/", MessageCreate.as_view(), name="add_message"),
+    path("home/messages_list/<int:pk>/edit_message/", MessageUpdateView.as_view(), name="edit_message"),
+    path("home/messages_list/<int:pk>/message_confirm_delete/", MessagesDeleteView.as_view(), name="delete_message")
 ]
