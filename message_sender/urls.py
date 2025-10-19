c54cbd3c3170b69b7fef237fd02b917836ef4220
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-19 17:09:20 +0500

    зарегистрированы пути для представлений mailing

--- message_sender/urls.py
+++ message_sender/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path("home/messages_list/", MessagesListView.as_view(), name="messages"),
     path("home/messages_list/add_message/", MessageCreate.as_view(), name="add_message"),
     path("home/messages_list/<int:pk>/edit_message/", MessageUpdateView.as_view(), name="edit_message"),
-    path("home/messages_list/<int:pk>/message_confirm_delete/", MessagesDeleteView.as_view(), name="delete_message")
-]
+    path("home/messages_list/<int:pk>/message_confirm_delete/", MessagesDeleteView.as_view(), name="delete_message"),
+    path("home/mailing_list/", MailingListView.as_view(), name="mailing"),
+    path("home/mailing_list/add_mailing/", MailingCreate.as_view(), name="add_mailing"),
+    path("home/mailing_list/<int:pk>/edit_mailing/", MailingUpdateView.as_view(), name="edit_mailing"),
+    path("home/mailing_list/<int:pk>/mailing_confirm_delete/", MailingDeleteView.as_view(), name="mailing_delete"),
+    ]
