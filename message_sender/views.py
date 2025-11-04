ea065d85be09332c50c0b92874eb6e1ce45be2df
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-04 19:03:42 +0500

    перенесены представления сообщений в приложение

--- message_sender/views.py
+++ message_sender/views.py
@@ -7,35 +7,6 @@ from .models import MailingRecipient, Message, Mailing
 
 
 # Create your views here.
-
-
-
-class MessageCreate(CreateView):
-    model = Message
-    fields = ['subject_line', 'message',]
-    template_name = 'message_sender/message_form.html'
-    success_url = reverse_lazy('sender:messages')
-
-
-class MessagesListView(ListView):
-    model = Message
-    template_name = 'message_sender/messages_list.html'
-    context_object_name = 'messages'
-
-
-class MessageUpdateView(UpdateView):
-    model = Message
-    fields = ['subject_line', 'message',]
-    template_name = 'message_sender/message_form.html'
-    success_url = reverse_lazy('sender:messages')
-
-
-class MessagesDeleteView(DeleteView):
-    model = Message
-    template_name = 'message_sender/message_confirm_delete.html'
-    success_url = reverse_lazy('sender:messages')
-
-
 class MailingCreate(CreateView):
     model = Mailing
     fields = ['start_date',
--- messages_for_clients/views.py
+++ messages_for_clients/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+
+from messages_for_clients.models import Message
+
 
 # Create your views here.
+class MessageCreate(CreateView):
+    model = Message
+    fields = ['subject_line', 'message',]
+    template_name = 'message_sender/message_form.html'
+    success_url = reverse_lazy('sender:messages')
+
+
+class MessagesListView(ListView):
+    model = Message
+    template_name = 'message_sender/messages_list.html'
+    context_object_name = 'messages'
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = ['subject_line', 'message',]
+    template_name = 'message_sender/message_form.html'
+    success_url = reverse_lazy('sender:messages')
+
+
+class MessagesDeleteView(DeleteView):
+    model = Message
+    template_name = 'message_sender/message_confirm_delete.html'
+    success_url = reverse_lazy('sender:messages')
