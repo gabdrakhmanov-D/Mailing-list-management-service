18471d50e5ddeefae54386dbd980edb0d63fc8bf
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-04 16:50:59 +0500

    модель получателя рассылки перенесена в приложение клиента

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class MailingRecipient(models.Model):
+
+    email = models.CharField(max_length=150,
+                             unique=True,
+                             blank=False,
+                             null=False,
+                             verbose_name="Получатель рассылки")
+
+    fullname = models.CharField(max_length=250,
+                                blank=False,
+                                null=False,
+                                verbose_name="Ф. И. О.")
+
+    comment = models.TextField(blank=True,
+                               null=True,
+                               verbose_name="Комментарий")
+
+    def __str__(self):
+        return f'{self.email} {self.fullname}'
+
+    class Meta:
+        verbose_name = 'Получатель'
+        verbose_name_plural = 'Получатели'
--- message_sender/models.py
+++ message_sender/models.py
@@ -1,29 +1,7 @@
 from django.db import models
 
 
-class MailingRecipient(models.Model):
 
-    email = models.CharField(max_length=150,
-                             unique=True,
-                             blank=False,
-                             null=False,
-                             verbose_name="Получатель рассылки")
-
-    fullname = models.CharField(max_length=250,
-                                blank=False,
-                                null=False,
-                                verbose_name="Ф. И. О.")
-
-    comment = models.TextField(blank=True,
-                               null=True,
-                               verbose_name="Комментарий")
-
-    def __str__(self):
-        return f'{self.email} {self.fullname}'
-
-    class Meta:
-        verbose_name = 'Получатель'
-        verbose_name_plural = 'Получатели'
 
 
 class Message(models.Model):
