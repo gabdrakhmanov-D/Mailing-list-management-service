57f1820a374538e128a54135d40220667e3330c3
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-04 15:02:50 +0500

    создана модель users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+    phone_number = models.CharField(max_length=15, verbose_name="Номер телефона", blank=True, null=True)
+    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
