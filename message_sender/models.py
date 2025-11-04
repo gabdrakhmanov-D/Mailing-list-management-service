279de38790c36ddee23b5ded907a469c9cad0d0a
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-04 16:55:14 +0500

    прописаны пути, зарег-но приложение

--- message_sender/models.py
+++ message_sender/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-
-
+from clients.models import MailingRecipient
 
 
 class Message(models.Model):
--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, TemplateView
 
