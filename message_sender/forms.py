99e2fe3ef38a9859833cea081a31ffdaf98131d6
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-06 22:09:11 +0500

    добавлен виджет для выбора даты и времени из календаря на странице

--- message_sender/forms.py
+++ message_sender/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from message_sender.models import Mailing
+from message_sender.widgets import DateTimePickerInput
 from users.utils import UserSettingUpMix
 
 
@@ -11,3 +12,8 @@ class MailingForm(UserSettingUpMix, forms.ModelForm):
         fields = ['message',
                   'recipients',
                   'end_date']
+
+    def __init__(self, *args, **kwargs):
+        super(MailingForm, self).__init__(*args, **kwargs)
+        self.fields['end_date'].widget = DateTimePickerInput()
+        self.fields['end_date'].input_formats = ['%d/%m/%Y %H:%M']
