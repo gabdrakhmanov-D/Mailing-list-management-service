6280c8ad74c63b0a9a63e3aaca9036346b7d4c4d
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-06 21:32:16 +0500

    в контроллер добавлены данные для отображения статистки отправки. Настроены таблицы

--- users/views.py
+++ users/views.py
@@ -2,12 +2,20 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, TemplateView
 
+from clients.models import MailingRecipient
+from message_sender.models import Mailing
 from users.forms import UserRegisterForm, UserSettingUpLoginForm, UserSettingUpProfile
 from users.models import User
 
 
 class HomeView(TemplateView):
     template_name = 'users/home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['mailing_count'] = Mailing.objects.count()
+        context['activ_mailing'] = Mailing.objects.filter(status='launched').count()
+        context['uniq_client_count'] = MailingRecipient.objects.distinct().count()
+        return context
 
 
 class RegisterView(CreateView):
