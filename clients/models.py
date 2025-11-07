9d7e7f95ad322225a6590086f2b5938ebf8d424d
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-07 14:24:51 +0500

    Создано поле автор добавления клиента, сделана миграция

--- clients/models.py
+++ clients/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from users.models import User
+
+
 class MailingRecipient(models.Model):
 
     email = models.CharField(max_length=150,
@@ -16,6 +19,8 @@ class MailingRecipient(models.Model):
     comment = models.TextField(blank=True,
                                null=True,
                                verbose_name="Комментарий")
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор добавления", related_name="clients")
 
     def __str__(self):
         return f'{self.email} {self.fullname}'
