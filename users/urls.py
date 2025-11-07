dc700dec2da7c5ca51d190407e4200111e07d454
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-07 14:08:28 +0500

    добавлены шаблоны и представления для сброса пароля. прописаны пути

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.views import LogoutView
-from django.urls import path
+from django.contrib.auth.views import LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
+from django.urls import path, reverse_lazy
 
 from users.apps import UsersConfig
 from users.views import RegisterView, UserLoginView, UserProfileEdit, HomeView, UserPasswordChange, \
@@ -15,4 +16,20 @@ urlpatterns = [
     path('profile/<int:pk>', UserProfileEdit.as_view(), name='profile'),
     path('password-change/', UserPasswordChange.as_view(), name='password_change'),
     path('password-change/done/', UserPasswordChangeDone.as_view(), name='password_change_done'),
+    path('password-reset/',
+         PasswordResetView.as_view(
+             template_name="users/password_reset_form.html",
+             email_template_name="users/password_reset_email.html",
+             success_url=reverse_lazy("users:password_reset_done")
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(template_name = "users/password_reset_done.html"),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html",
+             success_url=reverse_lazy("users:password_reset_complete")
+         ), name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name='password_reset_complete'),
 ]
