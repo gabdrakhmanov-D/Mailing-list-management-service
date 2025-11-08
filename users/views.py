de04fd6a047ec06bfd391fed6ce1d109d29430c4
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-08 16:48:41 +0500

    главный шаблон перенесен в отдельную папку, изменены пути

--- users/views.py
+++ users/views.py
@@ -17,7 +17,7 @@ from users.models import User
 
 @method_decorator(cache_page(60 * 2), name='dispatch')
 class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = 'users/home.html'
+    template_name = 'main/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
