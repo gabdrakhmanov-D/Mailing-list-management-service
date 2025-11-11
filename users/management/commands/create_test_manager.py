94898c9e1429d90389a29bb49c195f923c5f2021
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-11 11:46:31 +0500

    добавлены поля логин, почта и пароль при созданиее менеджера без группы

--- users/management/commands/create_test_manager.py
+++ users/management/commands/create_test_manager.py
@@ -6,13 +6,25 @@ from users.models import User
 
 class Command(BaseCommand):
 
-    help = 'Создание тестового менеджера'
+    help = 'Создание тестового менеджера. Обязательное указание через пробел: email username password.'
 
-    def handle(self, *args, **options):
+    def add_arguments(self, parser):
+        parser.add_argument('email',
+                            type=str,
+                            help='Электронная почта.')
 
-        email = "manager1@example.com"
-        username = "test_manager"
-        password = "1234"
+        parser.add_argument('username',
+                            type=str,
+                            help='Логин')
+
+        parser.add_argument('password',
+                            type=str,
+                            help='Пароль')
+
+    def handle(self, *args, **kwargs):
+        email = kwargs['email']
+        username = kwargs['username']
+        password = kwargs['password']
         user = User.objects.create(email=email, username=username)
         user.set_password(password)
         user.is_active = True
